0f4c8c06df32adfb9984d75b96eb04f9032f0957
Juan Carlos <jcrpelaez@yahoo.com>
2020-02-11 18:23:41 -0600

    web scraping functions

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3000;
+const hbs = require("express-hbs");
+
+// Web scrapping Tools
 const cheerio = require("cheerio");
 const axios = require("axios");
-let PORT = process.env.PORT || 3000;
-var hbs = require("express-hbs");
+
+// Require all models
+// const db = require("./models");
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/unit18Populater", {
+  useNewUrlParser: true
+});
 
 // Use `.hbs` for extensions and find partials in `views/partials`.
 app.engine(
@@ -20,4 +30,75 @@ app.get("/", function(req, res) {
   res.send("Hello World");
 });
 
-app.listen(PORT);
+// A GET route for scraping the echoJS website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with axios
+  axios.get("https://www.nytimes.com/").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+
+    $("").each(function(i, element) {
+      // Save an empty result object
+      var result = {};
+
+      result.title = $(this)
+        .children("")
+        .text();
+      result.link = $(this)
+        .children("")
+        .attr("");
+
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    });
+
+    res.send("Scrape Complete");
+  });
+});
+
+app.get("/articles", function(req, res) {
+  db.Article.find({})
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.get("/articles/:id", function(req, res) {
+  db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.post("/articles/:id", function(req, res) {
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      return db.Article.findOneAndUpdate(
+        { _id: req.params.id },
+        { note: dbNote._id },
+        { new: true }
+      );
+    })
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.listen(PORT, function() {
+  console.log("App running on port" + PORT + "!");
+});
