202d8a57e012bbc93506129c716d294763a8f900
Juan Carlos <jcrpelaez@yahoo.com>
2020-02-13 12:39:30 -0600

    added mongo/mLab connection

--- server.js
+++ server.js
@@ -1,104 +1,47 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const PORT = process.env.PORT || 3000;
-const hbs = require("express-hbs");
+var express = require("express");
+var app = express();
+var mongoose = require("mongoose");
+var PORT = process.env.PORT || 3000;
+var hbs = require("express-hbs");
+var logger = require("morgan");
 
 // Web scrapping Tools
-const cheerio = require("cheerio");
-const axios = require("axios");
+var cheerio = require("cheerio");
+var axios = require("axios");
 
-// Require all models
-// const db = require("./models");
+// Use morgan logger for logging requests
+app.use(logger("dev"));
 
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", {
+// Connect to the Mongo DB 
+mongoose.connect("mongodb://localhost/scraped-news", {
   useNewUrlParser: true
 });
+var db = mongoose.connection;
 
-// Use `.hbs` for extensions and find partials in `views/partials`.
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+  console.log("Connected to Mongoose!");
+});
+
+// Use `.hbs` for extensions and find partials in `views/layouts`.
 app.engine(
   "hbs",
   hbs.express4({
-    partialsDir: __dirname + "/views/partials"
+    defaultLayout: +"main"
   })
 );
 app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
 
-app.get("/", function(req, res) {
-  res.send("Hello World");
-});
-
-// A GET route for scraping the echoJS website
-app.get("/scrape", function(req, res) {
-  // First, we grab the body of the html with axios
-  axios.get("https://www.nytimes.com/").then(function(response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(response.data);
-
-    $("").each(function(i, element) {
-      // Save an empty result object
-      var result = {};
-
-      result.title = $(this)
-        .children("")
-        .text();
-      result.link = $(this)
-        .children("")
-        .attr("");
-
-      db.Article.create(result)
-        .then(function(dbArticle) {
-          console.log(dbArticle);
-        })
-        .catch(function(err) {
-          console.log(err);
-        });
-    });
-
-    res.send("Scrape Complete");
-  });
-});
-
-app.get("/articles", function(req, res) {
-  db.Article.find({})
-    .then(function(dbArticle) {
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
-});
-
-app.get("/articles/:id", function(req, res) {
-  db.Article.findOne({ _id: req.params.id })
-    .populate("note")
-    .then(function(dbArticle) {
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
-});
-
-app.post("/articles/:id", function(req, res) {
-  db.Note.create(req.body)
-    .then(function(dbNote) {
-      return db.Article.findOneAndUpdate(
-        { _id: req.params.id },
-        { note: dbNote._id },
-        { new: true }
-      );
-    })
-    .then(function(dbArticle) {
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
+app.get("main.hbs", function(req, res) {
+  res.send("");
 });
 
 app.listen(PORT, function() {
   console.log("App running on port" + PORT + "!");
 });
+
+var MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
